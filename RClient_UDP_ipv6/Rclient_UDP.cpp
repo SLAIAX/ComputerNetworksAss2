b8ceab9f2bcfd7018344790c4c706c09e31f672c
SLAIAX <1burgerboys@gmail.com>
2019-05-02 17:15:30 +1200

    Startup Code

---
+++ RClient_UDP_ipv6/Rclient_UDP.cpp
@@ -0,0 +1,245 @@
+//159.334 - Networks
+// CLIENT: prototype for assignment 2. 
+//Note that this progam is not yet cross-platform-capable
+// This code is different than the one used in previous semesters...
+//************************************************************************/
+//RUN WITH: Rclient_UDP 127.0.0.1 1235 0 0 
+//RUN WITH: Rclient_UDP 127.0.0.1 1235 0 1 
+//RUN WITH: Rclient_UDP 127.0.0.1 1235 1 0 
+//RUN WITH: Rclient_UDP 127.0.0.1 1235 1 1 
+//************************************************************************/
+
+//---
+#if defined __unix__ || defined __APPLE__
+	#include <unistd.h>
+	#include <errno.h>
+	#include <stdlib.h>
+	#include <stdio.h> 
+	#include <string.h>
+	#include <sys/types.h> 
+	#include <sys/socket.h> 
+	#include <arpa/inet.h>
+	#include <netinet/in.h> 
+	#include <netdb.h>
+    #include <iostream>
+#elif defined _WIN32 
+
+
+	//Ws2_32.lib
+	#define _WIN32_WINNT 0x501  //to recognise getaddrinfo()
+
+	//"For historical reasons, the Windows.h header defaults to including the Winsock.h header file for Windows Sockets 1.1. The declarations in the Winsock.h header file will conflict with the declarations in the Winsock2.h header file required by Windows Sockets 2.0. The WIN32_LEAN_AND_MEAN macro prevents the Winsock.h from being included by the Windows.h header"
+	#ifndef WIN32_LEAN_AND_MEAN
+	#define WIN32_LEAN_AND_MEAN
+	#endif
+
+	#include <winsock2.h>
+	#include <ws2tcpip.h>
+	#include <stdio.h> 
+	#include <string.h>
+	#include <stdlib.h>
+	#include <iostream>
+#endif
+
+#include "myrandomizer.h"
+
+using namespace std;
+
+#define WSVERS MAKEWORD(2,0)
+#define BUFFER_SIZE 80  //used by receive_buffer and send_buffer
+                        //the BUFFER_SIZE has to be at least big enough to receive the packet
+#define SEGMENT_SIZE 78
+//segment size, i.e., if fgets gets more than this number of bytes it segments the message into smaller parts.
+
+WSADATA wsadata;
+const int ARG_COUNT=5;
+//---
+int numOfPacketsDamaged=0;
+int numOfPacketsLost=0;
+int numOfPacketsUncorrupted=0;
+
+int packets_damagedbit=0;
+int packets_lostbit=0;
+
+/////////////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char *argv[]) {
+//*******************************************************************
+// Initialization
+//*******************************************************************
+   struct sockaddr_storage localaddr, remoteaddr;
+   char portNum[NI_MAXSERV];
+   struct addrinfo *result = NULL;
+   struct addrinfo hints;
+	
+   memset(&localaddr, 0, sizeof(localaddr));  //clean up
+   memset(&remoteaddr, 0, sizeof(remoteaddr));//clean up  
+   randominit();
+   SOCKET s;
+   char send_buffer[BUFFER_SIZE],receive_buffer[BUFFER_SIZE];
+   int n,bytes,addrlen;
+	
+   
+	addrlen=sizeof(struct sockaddr);
+	
+	
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_INET; //AF_INET6;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_protocol = IPPROTO_UDP;
+	
+//********************************************************************
+// WSSTARTUP
+//********************************************************************
+   if (WSAStartup(WSVERS, &wsadata) != 0) {
+      WSACleanup();
+      printf("WSAStartup failed\n");
+   }
+//*******************************************************************
+//	Dealing with user's arguments
+//*******************************************************************
+   if (argc != ARG_COUNT) {
+	   printf("USAGE: Rclient_UDP remote_IP-address remoteport allow_corrupted_bits(0 or 1) allow_packet_loss(0 or 1)\n");
+	   exit(1);
+   }
+	
+	int iResult=0;
+	
+	sprintf(portNum,"%s", argv[2]);
+	iResult = getaddrinfo(argv[1], portNum, &hints, &result);
+   
+   packets_damagedbit=atoi(argv[3]);
+   packets_lostbit=atoi(argv[4]);
+   if (packets_damagedbit < 0 || packets_damagedbit > 1 || packets_lostbit< 0 || packets_lostbit>1){
+	   printf("USAGE: Rclient_UDP remote_IP-address remoteport allow_corrupted_bits(0 or 1) allow_packet_loss(0 or 1)\n");
+	   exit(0);
+   }
+   
+//*******************************************************************
+//CREATE CLIENT'S SOCKET 
+//*******************************************************************
+   s = INVALID_SOCKET; 
+   s = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+   if (s == INVALID_SOCKET) {
+      printf("socket failed\n");
+   	exit(1);
+   }
+    //nonblocking option
+	// Set the socket I/O mode: In this case FIONBIO
+	// enables or disables the blocking mode for the 
+	// socket based on the numerical value of iMode.
+	// If iMode = 0, blocking is enabled; 
+	// If iMode != 0, non-blocking mode is enabled.
+   u_long iMode=1;
+	
+   iResult=ioctlsocket(s,FIONBIO,&iMode);
+   if (iResult != NO_ERROR){
+         printf("ioctlsocket failed with error: %d\n", iResult);
+		 closesocket(s);
+		 WSACleanup();
+		 exit(0);
+   }
+
+   cout << "==============<< UDP CLIENT >>=============" << endl;
+   cout << "channel can damage packets=" << packets_damagedbit << endl;
+   cout << "channel can lose packets=" << packets_lostbit << endl;
+	
+//*******************************************************************
+//SEND A TEXT FILE 
+//*******************************************************************
+   int counter=0;
+   char temp_buffer[BUFFER_SIZE];
+   FILE *fin=fopen("data_for_transmission.txt","rb"); //original
+	
+//In text mode, carriage return–linefeed combinations 
+//are translated into single linefeeds on input, and 
+//linefeed characters are translated to carriage return–linefeed combinations on output. 
+	
+   if(fin==NULL){
+	   printf("cannot open data_for_transmission.txt\n");
+	   closesocket(s);
+   	   WSACleanup();
+	   exit(0);
+   } else {
+	   printf("data_for_transmission.txt is now open for sending\n");
+	}
+   while (1){
+		memset(send_buffer, 0, sizeof(send_buffer));//clean up the send_buffer before reading the next line
+		if (!feof(fin)) {
+			fgets(send_buffer,SEGMENT_SIZE,fin); //get one line of data from the file
+			sprintf(temp_buffer,"PACKET %d ",counter);  //create packet header with Sequence number
+			counter++;
+			strcat(temp_buffer,send_buffer);   //append data to packet header
+			strcpy(send_buffer,temp_buffer);   //the complete packet
+			printf("\n======================================================\n");
+			cout << "calling send_unreliably, to deliver data of size " << strlen(send_buffer) << endl;
+			send_unreliably(s,send_buffer,(result->ai_addr)); //send the packet to the unreliable data channel
+			Sleep(1);  //sleep for 1 millisecond																			
+//********************************************************************
+//RECEIVE
+//********************************************************************
+			addrlen = sizeof(remoteaddr); //IPv4 & IPv6-compliant
+			bytes = recvfrom(s, receive_buffer, 78, 0,(struct sockaddr*)&remoteaddr,&addrlen);
+//********************************************************************
+//IDENTIFY server's IP address and port number.     
+//********************************************************************      
+	char serverHost[NI_MAXHOST]; 
+    char serverService[NI_MAXSERV];	
+    memset(serverHost, 0, sizeof(serverHost));
+    memset(serverService, 0, sizeof(serverService));
+
+
+    getnameinfo((struct sockaddr *)&remoteaddr, addrlen,
+                  serverHost, sizeof(serverHost),
+                  serverService, sizeof(serverService),
+                  NI_NUMERICHOST);
+
+
+
+    printf("\nReceived a packet of size %d bytes from <<<UDP Server>>> with IP address:%s, at Port:%s\n",bytes,serverHost, serverService); 	   
+	
+//********************************************************************
+//PROCESS REQUEST
+//********************************************************************
+			//Remove trailing CR and LN
+			if( bytes != SOCKET_ERROR ){	
+				n=0;
+				while (n<bytes){
+					n++;
+					if ((bytes < 0) || (bytes == 0)) break;	
+					if (receive_buffer[n] == '\n') { /*end on a LF*/
+						receive_buffer[n] = '\0';
+						break;
+					}
+					if (receive_buffer[n] == '\r') /*ignore CRs*/
+					receive_buffer[n] = '\0';
+				}
+				printf("RECEIVED --> %s, %d elements\n",receive_buffer, int(strlen(receive_buffer)));
+			}
+			
+		} else {
+			fclose(fin);
+			printf("End-of-File reached. \n"); 
+			memset(send_buffer, 0, sizeof(send_buffer)); 
+			sprintf(send_buffer,"CLOSE \r\n"); //send a CLOSE command to the RECEIVER (Server)
+			printf("\n======================================================\n");
+			
+			send_unreliably(s,send_buffer,(result->ai_addr));
+			break;
+		}
+		
+   } //while loop
+//*******************************************************************
+//CLOSESOCKET   
+//*******************************************************************
+   closesocket(s);
+   printf("Closing the socket connection and Exiting...\n");
+   cout << "==============<< STATISTICS >>=============" << endl;
+   cout << "numOfPacketsDamaged=" << numOfPacketsDamaged << endl;
+   cout << "numOfPacketsLost=" << numOfPacketsLost << endl;
+   cout << "numOfPacketsUncorrupted=" << numOfPacketsUncorrupted << endl;
+   cout << "===========================================" << endl;
+
+   exit(0);
+}
+	
---
+++ RServer_UDP_ipv6/Rserver_UDP.cpp
@@ -0,0 +1,299 @@
+//159.334 - Networks
+// SERVER: prototype for assignment 2.
+//Note that this progam is not yet cross-platform-capable
+// This code is different than the one used in previous semesters...
+//************************************************************************/
+//RUN WITH: Rserver_UDP 1235 0 0
+//RUN WITH: Rserver_UDP 1235 1 0 
+//RUN WITH: Rserver_UDP 1235 0 1 
+//RUN WITH: Rserver_UDP 1235 1 1  
+//************************************************************************/
+
+//---
+#if defined __unix__ || defined __APPLE__
+	#include <unistd.h>
+	#include <errno.h>
+	#include <stdlib.h>
+	#include <stdio.h> 
+	#include <string.h>
+	#include <sys/types.h> 
+	#include <sys/socket.h> 
+	#include <arpa/inet.h>
+	#include <netinet/in.h> 
+	#include <netdb.h>
+    #include <iostream>
+#elif defined _WIN32 
+
+
+	//Ws2_32.lib
+	#define _WIN32_WINNT 0x501  //to recognise getaddrinfo()
+
+	//"For historical reasons, the Windows.h header defaults to including the Winsock.h header file for Windows Sockets 1.1. The declarations in the Winsock.h header file will conflict with the declarations in the Winsock2.h header file required by Windows Sockets 2.0. The WIN32_LEAN_AND_MEAN macro prevents the Winsock.h from being included by the Windows.h header"
+	#ifndef WIN32_LEAN_AND_MEAN
+	#define WIN32_LEAN_AND_MEAN
+	#endif
+
+	#include <winsock2.h>
+	#include <ws2tcpip.h>
+	#include <stdio.h> 
+	#include <string.h>
+	#include <stdlib.h>
+	#include <iostream>
+#endif
+
+#include "myrandomizer.h" 
+
+using namespace std;
+
+#define BUFFER_SIZE 80  //used by receive_buffer and send_buffer
+                        //the BUFFER_SIZE needs to be at least big enough to receive the packet
+#define SEGMENT_SIZE 78
+
+const int ARG_COUNT=4;
+int numOfPacketsDamaged=0;
+int numOfPacketsLost=0;
+int numOfPacketsUncorrupted=0;
+
+int packets_damagedbit=0;
+int packets_lostbit=0;
+
+//*******************************************************************
+//Function to save lines and discard the header
+//*******************************************************************
+//You are allowed to change this. You will need to alter the NUMBER_OF_WORDS_IN_THE_HEADER if you add a CRC
+#define NUMBER_OF_WORDS_IN_THE_HEADER 2
+
+void save_line_without_header(char * receive_buffer,FILE *fout){
+	//char *sep = " "; //separator is the space character
+	
+	char sep[3];
+	
+    strcpy(sep," "); //separator is the space character
+	char *word;
+	int wcount=0;
+	char dataExtracted[BUFFER_SIZE]="\0";
+	
+	// strtok remembers the position of the last token extracted.
+	// strtok is first called using a buffer as an argument.
+	// successive calls requires NULL as an argument.
+	// the function remembers internally where it stopped last time
+	
+	//loop while word is not equal to NULL.
+	for (word = strtok(receive_buffer, sep); word; word = strtok(NULL, sep))
+	{
+		wcount++;
+		if(wcount > NUMBER_OF_WORDS_IN_THE_HEADER){ //if the word extracted is not part of the header anymore
+			strcat(dataExtracted, word); //extract the word and store it as part of the data
+			strcat(dataExtracted, " "); //append space
+		}
+	}
+	
+	dataExtracted[strlen(dataExtracted)-1]=(char)'\0'; //get rid of last space appended
+	printf("DATA: %s, %d elements\n",dataExtracted,int(strlen(dataExtracted)));
+
+	//make sure that the file pointer has been properly initialised
+	if (fout!=NULL) fprintf(fout,"%s\n",dataExtracted); //write to file
+	else {
+		printf("Error in writing to write...\n");
+		exit(1);
+	}
+}
+
+#define WSVERS MAKEWORD(2,0)
+WSADATA wsadata;
+
+//*******************************************************************
+//MAIN
+//*******************************************************************
+int main(int argc, char *argv[]) {
+//********************************************************************
+// INITIALIZATION
+//********************************************************************
+	struct sockaddr_storage clientAddress; //IPv4 & IPv6 -compliant
+	struct addrinfo *result = NULL;
+    struct addrinfo hints;
+	int iResult;
+	
+    SOCKET s;
+    char send_buffer[BUFFER_SIZE],receive_buffer[BUFFER_SIZE];
+    int n,bytes,addrlen;
+
+	memset(&hints, 0, sizeof(struct addrinfo));
+	 
+	hints.ai_family = AF_INET; //AF_INET6;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_protocol = IPPROTO_UDP;
+	hints.ai_flags = AI_PASSIVE; // For wildcard IP address 
+	
+	
+    randominit();
+//********************************************************************
+// WSSTARTUP
+//********************************************************************
+   if (WSAStartup(WSVERS, &wsadata) != 0) {
+      WSACleanup();
+      printf("WSAStartup failed\n");
+   }
+	
+	if (argc != ARG_COUNT) {
+	   printf("USAGE: Rserver_UDP localport allow_corrupted_bits(0 or 1) allow_packet_loss(0 or 1)\n");
+	   exit(1);
+   }
+   
+	iResult = getaddrinfo(NULL, argv[1], &hints, &result); //converts human-readable text strings representing hostnames or IP addresses 
+	                                                        //into a dynamically allocated linked list of struct addrinfo structures
+																			  //IPV4 & IPV6-compliant
+ 
+	if (iResult != 0) {
+    printf("getaddrinfo failed: %d\n", iResult);
+    WSACleanup();
+    return 1;
+   }	
+	
+//********************************************************************
+//SOCKET
+//********************************************************************
+   s = INVALID_SOCKET; //socket for listening
+	// Create a SOCKET for the server to listen for client connections
+
+	s = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+	//check for errors in socket allocation
+	if (s == INVALID_SOCKET) {
+		 printf("Error at socket(): %d\n", WSAGetLastError());
+		 freeaddrinfo(result);
+		 WSACleanup();
+		 exit(1);//return 1;
+	}
+	
+   
+   
+	
+   packets_damagedbit=atoi(argv[2]);
+   packets_lostbit=atoi(argv[3]);
+   if (packets_damagedbit < 0 || packets_damagedbit > 1 || packets_lostbit < 0 || packets_lostbit > 1){
+	   printf("USAGE: Rserver_UDP localport allow_corrupted_bits(0 or 1) allow_packet_loss(0 or 1)\n");
+	   exit(0);
+   }
+      
+   int counter=0;
+//********************************************************************
+//BIND
+//********************************************************************
+   iResult = bind( s, result->ai_addr, (int)result->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        printf("bind failed with error: %d\n", WSAGetLastError());
+        freeaddrinfo(result);
+		 
+        closesocket(s);
+        WSACleanup();
+        return 1;
+    }
+
+    cout << "==============<< UDP SERVER >>=============" << endl;
+    cout << "channel can damage packets=" << packets_damagedbit << endl;
+    cout << "channel can lose packets=" << packets_lostbit << endl;
+	 
+	 freeaddrinfo(result); //free the memory allocated by the getaddrinfo 
+	                       //function for the server's address, as it is 
+	                       //no longer needed
+//********************************************************************
+// Open file to save the incoming packets
+//********************************************************************
+//In text mode, carriage return–linefeed combinations 
+//are translated into single linefeeds on input, and 
+//linefeed characters are translated to carriage return–linefeed combinations on output.    
+	 FILE *fout=fopen("data_received.txt","w"); 
+	 
+//********************************************************************
+	 
+//INFINITE LOOP
+//********************************************************************
+   while (1) {
+//********************************************************************
+//RECEIVE
+//********************************************************************
+//printf("Waiting... \n");		
+		addrlen = sizeof(clientAddress); //IPv4 & IPv6-compliant
+		memset(receive_buffer,0,sizeof(receive_buffer));
+		bytes = recvfrom(s, receive_buffer, SEGMENT_SIZE, 0, (struct sockaddr*)&clientAddress, &addrlen);
+
+//********************************************************************
+//IDENTIFY UDP client's IP address and port number.     
+//********************************************************************      
+	char clientHost[NI_MAXHOST]; 
+    char clientService[NI_MAXSERV];	
+    memset(clientHost, 0, sizeof(clientHost));
+    memset(clientService, 0, sizeof(clientService));
+
+
+    getnameinfo((struct sockaddr *)&clientAddress, addrlen,
+                  clientHost, sizeof(clientHost),
+                  clientService, sizeof(clientService),
+                  NI_NUMERICHOST);
+
+
+
+    printf("\nReceived a packet of size %d bytes from <<<UDP Client>>> with IP address:%s, at Port:%s\n",bytes,clientHost, clientService); 	   
+		
+//********************************************************************
+//PROCESS RECEIVED PACKET
+//********************************************************************
+		
+		//Remove trailing CR and LF
+		n=0;
+		while(n < bytes){
+			n++;
+			if ((bytes < 0) || (bytes == 0)) break;
+			if (receive_buffer[n] == '\n') { /*end on a LF*/
+				receive_buffer[n] = '\0';
+				break;
+			}
+			if (receive_buffer[n] == '\r') /*ignore CRs*/
+				receive_buffer[n] = '\0';
+		}
+		
+		if ((bytes < 0) || (bytes == 0)) break;
+		
+		printf("\n================================================\n");	
+		printf("RECEIVED --> %s \n",receive_buffer);
+		
+		if (strncmp(receive_buffer,"PACKET",6)==0)  {
+			sscanf(receive_buffer, "PACKET %d",&counter);
+//********************************************************************
+//SEND ACK
+//********************************************************************
+			sprintf(send_buffer,"ACK %d \r\n",counter);
+			
+			//send ACK ureliably
+		
+			send_unreliably(s,send_buffer,(sockaddr*)&clientAddress );  
+			
+			//store the packet's data into a file
+			save_line_without_header(receive_buffer,fout);
+		}
+		else {
+			if (strncmp(receive_buffer,"CLOSE",5)==0)  {//if client says "CLOSE", the last packet for the file was sent. Close the file
+				//Remember that the packet carrying "CLOSE" may be lost or damaged as well!
+				fclose(fout);
+				closesocket(s);
+				printf("Server saved data_received.txt \n");//you have to manually check to see if this file is identical to file1_Windows.txt
+				printf("Closing the socket connection and Exiting...\n");
+				break;
+			}
+			else {//it is not a PACKET nor a CLOSE; therefore, it might be a damaged packet
+				   //Are you going to do nothing, ignoring the damaged packet? 
+				   //Or, send a negative ACK? It is up to you to decide here.
+			}
+		}
+   }
+   closesocket(s);
+
+   cout << "==============<< STATISTICS >>=============" << endl;
+   cout << "numOfPacketsDamaged=" << numOfPacketsDamaged << endl;
+   cout << "numOfPacketsLost=" << numOfPacketsLost << endl;
+   cout << "numOfPacketsUncorrupted=" << numOfPacketsUncorrupted << endl;
+   cout << "===========================================" << endl;
+
+   exit(0);
+}
